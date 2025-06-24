8f34026a1809b068a156e391a9cb4298cc90f8f9
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2025-06-25 00:16:31 +0200

    ingredients don't store quantity data

---
+++ src/main/java/io/enderdev/catalyx/client/gui/ButtonSide.kt
@@ -0,0 +1,5 @@
+package io.enderdev.catalyx.client.gui
+
+enum class ButtonSide(val xMult: Int) {
+	LEFT(1), RIGHT(-1);
+}
--- src/main/java/io/enderdev/catalyx/utils/extensions/Item.kt
+++ src/main/java/io/enderdev/catalyx/utils/extensions/Item.kt
@@ -6,4 +6,4 @@ import net.minecraft.item.crafting.Ingredient
 
 fun Item.toStack(quantity: Int = 1, meta: Int = 0) = ItemStack(this, quantity, meta)
 
-fun Item.toIngredient(quantity: Int = 1, meta: Int = 0): Ingredient = Ingredient.fromStacks(toStack(quantity, meta))
+fun Item.toIngredient(meta: Int = 0): Ingredient = Ingredient.fromStacks(toStack(meta = meta))
