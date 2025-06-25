f3897b472f9f70d3f202e927e93a4c7cf66e66d7
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2025-06-25 18:53:59 +0200

    wut

--- src/main/java/io/enderdev/catalyx/utils/extensions/Item.kt
+++ src/main/java/io/enderdev/catalyx/utils/extensions/Item.kt
@@ -7,4 +7,4 @@ import net.minecraft.item.crafting.Ingredient
 fun Item.toStack(quantity: Int = 1, meta: Int = 0) = ItemStack(this, quantity, meta)
 
 fun Item.toIngredient(meta: Int = 0): Ingredient = Ingredient.fromStacks(toStack(meta = meta))
-=
+
