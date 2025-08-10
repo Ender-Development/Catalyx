c3f21b52103cedb74f7efa733b0ca5b650f87cef
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2025-08-10 19:19:40 +0200

    add utility ASM class for animation

---
+++ src/main/java/io/enderdev/catalyx/animation/NoopAnimationStateMachine.kt
@@ -0,0 +1,27 @@
+package io.enderdev.catalyx.animation
+
+import com.google.common.collect.ImmutableMap
+import io.enderdev.catalyx.utils.SideUtils
+import net.minecraft.util.ResourceLocation
+import net.minecraftforge.client.model.ModelLoaderRegistry
+import net.minecraftforge.common.animation.ITimeValue
+import net.minecraftforge.common.model.animation.IAnimationStateMachine
+
+class NoopAnimationStateMachine() : IAnimationStateMachine {
+	companion object {
+		fun loadASM(location: ResourceLocation, customParameters: Map<String, ITimeValue>) =
+			if(SideUtils.isDedicatedServer)
+				NoopAnimationStateMachine()
+			else
+				ModelLoaderRegistry.loadASM(location, ImmutableMap.copyOf(customParameters))
+	}
+
+	override fun apply(time: Float) = null
+	override fun shouldHandleSpecialEvents(value: Boolean) {}
+
+	private var state: String? = null
+	override fun transition(newState: String?) {
+		state = newState
+	}
+	override fun currentState() = state
+}
