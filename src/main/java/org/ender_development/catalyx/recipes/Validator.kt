cb9115d8ce1dc69d0c0760b60b266462d60d8c1c
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-09-14 00:33:47 +0200

    proper validator

---
+++ src/main/java/org/ender_development/catalyx/recipes/Validator.kt
@@ -0,0 +1,18 @@
+package org.ender_development.catalyx.recipes
+
+import org.ender_development.catalyx.Catalyx
+
+class Validator {
+	private val errorMessages = mutableListOf<String>()
+
+	val isValid: Boolean
+		get() = errorMessages.isEmpty()
+
+	fun error(message: String) = errorMessages.add(message)
+
+	fun getMessage(): String = errorMessages.joinToString("\n")
+
+	fun listMessages(): List<String> = errorMessages.toList()
+
+	fun logMessages() = errorMessages.forEach { Catalyx.logger.error(it) }
+}
