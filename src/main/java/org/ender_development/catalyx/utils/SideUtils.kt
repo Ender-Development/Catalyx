9107985199d3fd1dea0b1c305d4837a3b4cb7f99
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2025-09-14 09:25:28 +0200

    move SideUtils EntityPlayer checks into a util extension

--- src/main/java/org/ender_development/catalyx/utils/SideUtils.kt
+++ src/main/java/org/ender_development/catalyx/utils/SideUtils.kt
@@ -1,8 +1,5 @@
 package org.ender_development.catalyx.utils
 
-import net.minecraft.client.entity.EntityPlayerSP
-import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraftforge.fml.common.FMLCommonHandler
 
 /**
@@ -11,18 +8,8 @@ import net.minecraftforge.fml.common.FMLCommonHandler
 object SideUtils {
 	private val handler = FMLCommonHandler.instance()
 
-	val isClient: Boolean = handler.effectiveSide.isClient
-	val isServer: Boolean = handler.effectiveSide.isServer
-	val isDedicatedClient: Boolean = handler.side.isClient
-	val isDedicatedServer: Boolean = handler.side.isServer
-
-	fun isClient(player: EntityPlayer?): Boolean {
-		if (player == null) throw NullPointerException("Can't get the side of a null player!")
-		return if (player.world == null) player is EntityPlayerSP else player.world.isRemote
-	}
-
-	fun isServer(player: EntityPlayer?): Boolean {
-		if (player == null) throw NullPointerException("Can't get the side of a null player!")
-		return if (player.world == null) player is EntityPlayerMP else !player.world.isRemote
-	}
+	val isClient = handler.effectiveSide.isClient
+	val isServer = handler.effectiveSide.isServer
+	val isDedicatedClient = handler.side.isClient
+	val isDedicatedServer = handler.side.isServer
 }
