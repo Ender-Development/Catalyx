2e66b4da2bbd0ede116771adbacb65e28da40a1e
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2025-10-23 16:09:16 +0200

    add some more ByteBuf extensions

--- src/main/java/org/ender_development/catalyx/utils/extensions/ByteBuf.kt
+++ src/main/java/org/ender_development/catalyx/utils/extensions/ByteBuf.kt
@@ -3,6 +3,8 @@
 package org.ender_development.catalyx.utils.extensions
 
 import io.netty.buffer.ByteBuf
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NBTTagCompound
 import net.minecraftforge.fml.common.network.ByteBufUtils
 
 /**
@@ -16,3 +18,15 @@ inline fun ByteBuf.writeString(value: String) =
  */
 inline fun ByteBuf.readString(): String =
 	ByteBufUtils.readUTF8String(this)
+
+inline fun ByteBuf.writeItemStack(stack: ItemStack) =
+	ByteBufUtils.writeItemStack(this, stack)
+
+inline fun ByteBuf.readItemStack(): ItemStack =
+	ByteBufUtils.readItemStack(this)
+
+inline fun ByteBuf.writeTagCompound(tag: NBTTagCompound) =
+	ByteBufUtils.writeTag(this, tag)
+
+inline fun ByteBuf.readTagCompound(): NBTTagCompound? =
+	ByteBufUtils.readTag(this)
