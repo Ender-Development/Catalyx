5210c759257454a4b5cdc138951bfabff21e1119
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-10-24 14:42:21 +0200

    more boilerplate code and a useful AABB extension

--- src/main/java/org/ender_development/catalyx/blocks/multiblock/parts/AbstractEdgeBlock.kt
+++ src/main/java/org/ender_development/catalyx/blocks/multiblock/parts/AbstractEdgeBlock.kt
@@ -6,12 +6,15 @@ import net.minecraft.block.properties.PropertyInteger
 import net.minecraft.block.state.BlockStateContainer
 import net.minecraft.block.state.IBlockState
 import net.minecraft.creativetab.CreativeTabs
+import net.minecraft.entity.Entity
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.NonNullList
+import net.minecraft.util.math.AxisAlignedBB
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.RayTraceResult
+import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
 import org.ender_development.catalyx.blocks.BaseBlock
 import org.ender_development.catalyx.blocks.multiblock.*
@@ -79,4 +82,16 @@ abstract class AbstractEdgeBlock(mod: ICatalyxMod, val name: String) : BaseBlock
 	@Deprecated("Implementation is fine.")
 	override fun getPushReaction(state: IBlockState): EnumPushReaction =
 		EnumPushReaction.BLOCK
+
+	@Deprecated("Implementation is fine")
+    override fun getBoundingBox(state: IBlockState, source: IBlockAccess, pos: BlockPos): AxisAlignedBB = getAABB(state)
+
+    @Deprecated("Implementation is fine")
+    override fun getCollisionBoundingBox(blockState: IBlockState, worldIn: IBlockAccess, pos: BlockPos): AxisAlignedBB? = getAABB(blockState)
+
+    @Deprecated("Implementation is fine")
+    override fun addCollisionBoxToList(state: IBlockState, worldIn: World, pos: BlockPos, entityBox: AxisAlignedBB, collidingBoxes: List<AxisAlignedBB>, entity: Entity?, isActualState: Boolean) {
+        @Suppress("DEPRECATION")
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, getAABB(state))
+    }
 }
