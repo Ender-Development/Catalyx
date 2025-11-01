8fbfd61b236f26b06438407f5d16d4864abefa3b
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2025-11-01 11:39:07 +0100

    change Color.destruct() to Color.componentN()

--- src/main/java/org/ender_development/catalyx/utils/extensions/Color.kt
+++ src/main/java/org/ender_development/catalyx/utils/extensions/Color.kt
@@ -5,13 +5,22 @@ package org.ender_development.catalyx.utils.extensions
 import java.awt.Color
 
 inline fun Color.withAlpha(alpha: Float) =
-	withAlpha((alpha * 255.0f).toInt())
+	withAlpha((alpha * 255f).toInt())
 
 inline fun Color.withAlpha(alpha: Int) =
 	Color(red, green, blue, alpha)
 
-inline fun Color.destruct() =
-	arrayOf(red, green, blue, alpha)
+inline operator fun Color.component1() =
+	red
+
+inline operator fun Color.component2() =
+	green
+
+inline operator fun Color.component3() =
+	blue
+
+inline operator fun Color.component4() =
+	alpha
 
 inline fun Color.destructFloat() =
 	floatArrayOf(red / 255f, green / 255f, blue / 255f, alpha / 255f)
