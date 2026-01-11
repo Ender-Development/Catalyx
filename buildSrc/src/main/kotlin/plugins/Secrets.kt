950e179cce3844b40e4f0edf71355916ece8df26
Fabian Rehhauße <rklebestreifen@yahoo.com>
2026-01-11 02:20:25 +0100

    Update buildSrc to fix syncing (#10)

--- buildSrc/src/main/kotlin/plugins/Secrets.kt
+++ buildSrc/src/main/kotlin/plugins/Secrets.kt
@@ -14,7 +14,7 @@ class Secrets : Plugin<Project> {
          *
          * @param name The name of the property to load from the secrets.properties file.
          */
-        fun get(name: String) = Loader.getPropertyFromFile(PROPERTIES_FILE, name)?.toString()
+        fun get(name: String) = Loader.getPropertyFromFile(PROPERTIES_FILE, name)
 
         /**
          * Loads properties from the specified properties file located in the resources' directory.
@@ -23,17 +23,45 @@ class Secrets : Plugin<Project> {
          * @param name The name of the property to load from the secrets.properties file or environment variables.
          * @return The value of the property or environment variable, or null if not found.
          */
-        fun getOrEnvironment(name: String) = get(name) ?: System.getenv(name)?.toString()
+        fun getOrEnvironment(name: String): String? = get(name) ?: System.getenv(name)
     }
 
     override fun apply(target: Project) {
-        Logger.greet(this)
-        if (!target.rootProject.file(PROPERTIES_FILE).exists() && target.rootProject.file(EXAMPLE_FILE).exists()) {
-            Logger.warn("No '$PROPERTIES_FILE' file found in the project root. Please create one based on '$EXAMPLE_FILE'.")
-            println("WARNING: No '$PROPERTIES_FILE' file found in the project root. Please create one based on '$EXAMPLE_FILE'.")
-        }
-        if (File(PROPERTIES_FILE).exists()) {
-            Loader.loadPropertyFile(PROPERTIES_FILE)
-        }
+	    Logger.greet(this)
+
+	    val rootSecrets = target.rootProject.file(PROPERTIES_FILE)
+	    val userSecrets = File(
+		    System.getProperty("user.home"),
+		    ".gradle/$PROPERTIES_FILE"
+	    )
+	    // Searches for PROPERTIES_FILE in ~/.gradle/
+	    //     -> No need to store secrets inside the repo tree anymore BECAUSE IT IS FCKN DANGEROUS!
+	    //        Even with .gitignore!
+	    //        Thx, Klebi <3
+
+
+	    val secretsFile = when {
+		    rootSecrets.exists() -> {
+			    if (!getOrEnvironment("DISMISS_SECRET_FILE_WARNING").toBoolean()) {
+				    Logger.warn("Don't store secrets inside the repo tree BECAUSE IT IS FCKN DANGEROUS!")
+			    }
+				rootSecrets
+		    }
+		    userSecrets.exists() -> userSecrets
+		    else -> null
+	    }
+
+	    if (secretsFile == null) {
+		    if (target.rootProject.file(EXAMPLE_FILE).exists()) {
+			    Logger.warn("No '$PROPERTIES_FILE' found. Please create one in project root or even better in ~/.gradle based on '$EXAMPLE_FILE'.")
+
+				// Needed?
+				//println("WARNING: No '$PROPERTIES_FILE' found in project root or ~/.gradle. Please create one based on '$EXAMPLE_FILE'.")
+		    }
+		    return
+	    }
+
+	    Logger.info("Loading secrets from: ${secretsFile.absolutePath}")
+	    Loader.loadPropertyFile(secretsFile.absolutePath)
     }
 }
