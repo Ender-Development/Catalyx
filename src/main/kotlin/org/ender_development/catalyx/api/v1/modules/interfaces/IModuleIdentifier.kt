d8ac81c81b2f6e294e28293d667a2c485f1482fa
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2026-01-13 20:24:16 +0100

    IModuleIdentifier#toResourceLocation

--- src/main/kotlin/org/ender_development/catalyx/api/v1/modules/interfaces/IModuleIdentifier.kt
+++ src/main/kotlin/org/ender_development/catalyx/api/v1/modules/interfaces/IModuleIdentifier.kt
@@ -1,9 +1,17 @@
 package org.ender_development.catalyx.api.v1.modules.interfaces
 
+import net.minecraft.util.ResourceLocation
+
 /**
  * An identifier thet identifies a module
  */
 interface IModuleIdentifier {
 	val containerId: String
 	val moduleId: String
+
+	/**
+	 * Convert this to a [ResourceLocation], for whatever reason.
+	 */
+	fun toResourceLocation() =
+		ResourceLocation(containerId, moduleId)
 }
