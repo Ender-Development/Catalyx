919d94dba5c7c3a4049855cdb40d88fd5dd35b4a
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2026-01-23 22:29:33 +0100

    eat SideUtils into Utils

--- src/main/kotlin/org/ender_development/catalyx/api/v1/utils/SideUtils.kt
+++
@@ -1,5 +0,0 @@
-package org.ender_development.catalyx.api.v1.utils
-
-import org.ender_development.catalyx.api.v1.utils.interfaces.ISideUtils
-
-val SideUtils: ISideUtils = org.ender_development.catalyx.core.utils.SideUtils
