d78e73a6d9d8fe5362f97668192d57d6dfe4a04c
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2026-06-02 17:25:59 +0200

    what

--- src/main/kotlin/org/ender_development/catalyx/core/common/config/ConfigEntry.kt
+++ src/main/kotlin/org/ender_development/catalyx/core/common/config/ConfigEntry.kt
@@ -4,13 +4,13 @@ import net.minecraft.block.Block
 import net.minecraft.block.state.IBlockState
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
-import org.ender_development.catalyx.api.v1.common.extensions.toBlock
-import org.ender_development.catalyx.api.v1.common.extensions.toBlockState
-import org.ender_development.catalyx.api.v1.common.extensions.toItem
-import org.ender_development.catalyx.api.v1.common.extensions.toStack
-import java.util.Objects
+import org.ender_development.catalyx.api.v1.common.extensions.*
+import java.util.*
 
-open class GenericConfigEntry<T> {
+/**
+ * A generic class that takes in an input with the form "mod:id:meta;value", and parses it out into [mod], [id], [meta] and [value] (both value and meta are considered optional)
+ */
+open class GenericConfigEntry<T>(input: String, parser: (String) -> T) {
 	companion object {
 		const val IGNORE_META = -1
 	}
@@ -32,39 +32,37 @@ open class GenericConfigEntry<T> {
 	val blockState: IBlockState?
 		get() = "$mod:$id".toBlockState(if(meta == IGNORE_META) 0 else meta)
 
-	constructor(input: String, parser: (String) -> T) {
-		var split = input.split(";")
-		if(split.size > 2)
-			error("Invalid Config Entry: $input")
-		value = if(split.size == 2)
-			parser.invoke(split[1])
-		else
-			null
-		split = split[0].split(":")
-		if(split.size !in 2..3)
-			error("Invalid Config Entry: $input")
-		mod = split[0]
-		id = split[1]
-		meta = if(split.size != 3 || split[2] == "-1")
-			IGNORE_META
-		else
-			split[2].toInt()
+	init {
+		// input is "mod:id:meta;value", both value and meta are optional
+		val split = input.split(';')
+		value = when(split.size) {
+			1 -> null
+			2 -> parser(split[1])
+			else -> error("Invalid config entry - too many or too few semicolon-separated fields: '$input'")
+		}
+
+		val itemSplit = split[0].split(':')
+		if(itemSplit.size != 2 && itemSplit.size != 3)
+			error("Invalid config entry - too many or too few colon-separated fields: '$input'")
+
+		mod = itemSplit[0]
+		id = itemSplit[1]
+		meta = itemSplit.getApplyOrDefault(2, String::toInt, ::IGNORE_META)
 	}
 
-	override fun equals(other: Any?): Boolean {
-		return when (other) {
+	override fun equals(other: Any?) =
+		this === other || when(other) {
 			is Item -> other == item
 			is ItemStack -> if(meta == IGNORE_META)
-					other.isItemEqualIgnoreDurability(itemStack)
-				else
-					other.isItemEqual(itemStack)
+				other.isItemEqualIgnoreDurability(itemStack)
+			else
+				other.isItemEqual(itemStack)
 			is Block -> other == block
 			is IBlockState -> other == blockState
-			else -> this === other
+			else -> false
 		}
-	}
 
-	override fun hashCode(): Int =
+	override fun hashCode() =
 		Objects.hash(mod, id, meta, value)
 }
 
